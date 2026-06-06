e9819ac19aad023a3849ce9bf65afdf2fc279c7f
gugag123 <damascenogustavo65@gmail.com>
2026-06-05 23:39:20 -0300

    Add files via upload

---
+++ Autenticador.java
@@ -0,0 +1,23 @@
+package Sistema_De_Delivery_De_Restaurante.entities;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private int codigo;
+
+    public Produto( int codigo,String nome, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public double getPreco() {
+        return preco;
+    }
+}
---
+++ Produto.java
@@ -0,0 +1,20 @@
+package Sistema_De_Delivery_De_Restaurante.entities;
+
+public class Usuario {
+    private String login;
+    private String senha;
+
+    public Usuario(String login, String senha) {
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public String getLogin() {
+
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}
